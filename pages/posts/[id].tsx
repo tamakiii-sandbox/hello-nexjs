5da975db4903c90c796005e97e9109e885b96c82
tamakiii <tamakiii@users.noreply.github.com>
2020-05-07 16:59:46 +0900

    typescript

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,9 +3,9 @@ import Layout from '../../components/layout'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
 
-
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds();
   return {
     paths,
@@ -13,7 +13,7 @@ export async function getStaticPaths() {
   }
 }
 
-export async function getStaticProps({ params }) {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const postData = await getPostData(params.id)
   return {
     props: {
